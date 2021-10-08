588baf8367c8373695c670addd80035dc9279d7c
Anas ID SALEH <anasidsaleh55@gmail.com>
2021-10-08 04:51:26 +0100

    adding the qualification(still a problem to fix)  skills and project section

--- js/main.js
+++ js/main.js
@@ -30,16 +30,58 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content')
+const skillsHeader = document.querySelectorAll('.skills__header')
 
+function toggleSkills(){
+    let itemClass = this.parentNode.className
 
+    for(i = 0; i < skillsContent.length; i++){
+        skillsContent[i].className = 'skills__content skills__close'
+    }
+    if(itemClass === 'skills__content skills__close'){
+        this.parentNode.className = 'skills__content skills__open'
+    }
+}
+
+skillsHeader.forEach((el) =>{
+    el.addEventListener('click', toggleSkills)
+})
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-traget]')
+const tabContents = document.querySelectorAll('[data-content]')
 
 
-/*==================== SERVICES MODAL ====================*/
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+var swiper = new Swiper(".mySwiper", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
